624fd3932fb9f5ae6629a42f82718d422f70e129
poggy33 <33poggy33@gmail.com>
2026-04-04 21:28:15 +0300

    before popup after cluster and hover

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -31,55 +31,57 @@ export default function Map() {
   };
 
   const buildGeoJSON = (): FeatureCollection<Point> => {
-  const filtered = properties.filter(
-    (p) => p.dealType === dealType && p.propertyType === propertyType
-  );
+    const filtered = properties.filter(
+      (p) => p.dealType === dealType && p.propertyType === propertyType,
+    );
 
-  return {
-    type: "FeatureCollection",
-    features: filtered.map((p) => {
-      let label = "";
+    return {
+      type: "FeatureCollection",
+      features: filtered.map((p) => {
+        let label = "";
 
-      if (p.propertyType === "apartment") {
-        if (p.dealType === "sale") {
-          const pricePerSqm = Math.round(p.price / p.area);
+        if (p.propertyType === "apartment") {
+          if (p.dealType === "sale") {
+            const pricePerSqm = Math.round(p.price / p.area);
 
+            label =
+              `$${p.price.toLocaleString()}\n` +
+              `${p.rooms} кімн.\n` +
+              `$${pricePerSqm}/m²`;
+          } else {
+            label = `$${p.price}/міс\n${p.rooms} кімн.`;
+          }
+        }
+
+        if (p.propertyType === "house") {
           label =
             `$${p.price.toLocaleString()}\n` +
             `${p.rooms} кімн.\n` +
-            `$${pricePerSqm}/m²`;
-        } else {
-          label = `$${p.price}/міс\n${p.rooms} кімн.`;
+            `${p.floors} пов.`;
         }
-      }
 
-      if (p.propertyType === "house") {
-        label =
-          `$${p.price.toLocaleString()}\n` +
-          `${p.rooms} кімн.\n` +
-          `${p.floors} пов.`;
-      }
+        if (p.propertyType === "land") {
+          const perSotka = Math.round(p.price / p.area);
+          label =
+            `$${p.price.toLocaleString()}\n` +
+            `${p.area} сот.\n` +
+            `${formatToK(perSotka)}/сот.`;
+        }
 
-      if (p.propertyType === "land") {
-        const perSotka = Math.round(p.price / p.area);
-        label =
-          `$${p.price.toLocaleString()}\n` +
-          `${p.area} сот.\n` +
-          `${formatToK(perSotka)}/сот.`;
-      }
-
-      return {
-        type: "Feature",
-        id: p.id,
-        properties: { label },
-        geometry: {
-          type: "Point",
-          coordinates: p.coordinates,
-        },
-      };
-    }),
+        return {
+          type: "Feature",
+          properties: {
+            label,
+            id: p.id, // 🔥 ДОДАЙ
+          },
+          geometry: {
+            type: "Point",
+            coordinates: p.coordinates,
+          },
+        };
+      }),
+    };
   };
-};
 
   // 🔥 створення карти (ОДИН РАЗ)
   useEffect(() => {
@@ -98,12 +100,58 @@ export default function Map() {
     mapRef.current = map;
 
     map.on("load", () => {
-      const geojson = buildGeoJSON(); // 🔥 дефолт
-
+      // const geojson = buildGeoJSON(); // 🔥 дефолт
+      map.resize();
       map.addSource("points", {
         type: "geojson",
-        data: geojson,
-        generateId: true, // 🔥 ДОДАЙ ЦЕ
+        data: buildGeoJSON(), // 🔥 ВАЖЛИВО
+        // generateId: true,
+        cluster: true,
+        clusterMaxZoom: 13,
+        clusterRadius: 50,
+      });
+
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "points",
+
+        // 👇 показує тільки кластери
+        filter: ["has", "point_count"],
+
+        paint: {
+          "circle-color": "#2563eb",
+
+          // 👇 розмір залежить від кількості
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20, // <10
+            10,
+            25, // 10-30
+            30,
+            30, // 30+
+          ],
+
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#fff",
+        },
+      });
+
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "points",
+        filter: ["has", "point_count"],
+
+        layout: {
+          "text-field": ["get", "point_count_abbreviated"],
+          "text-size": 14,
+        },
+
+        paint: {
+          "text-color": "#fff",
+        },
       });
 
       // 🔵 фон
@@ -111,6 +159,7 @@ export default function Map() {
         id: "badge-bg",
         type: "circle",
         source: "points",
+        filter: ["!", ["has", "point_count"]],
         paint: {
           "circle-radius": 18,
           "circle-color": "#ffffff",
@@ -120,24 +169,29 @@ export default function Map() {
         minzoom: 13,
       });
 
+      map.addLayer({
+        id: "hover-layer",
+        type: "circle",
+        source: "points",
+        filter: ["==", ["get", "id"], -1], // нічого не показує
+
+        paint: {
+          "circle-radius": 24,
+          "circle-color": "#ffffff",
+          "circle-stroke-width": 3,
+          "circle-stroke-color": "#000",
+        },
+      });
+
       // 🔵 точки
       map.addLayer({
         id: "points-layer",
         type: "circle",
         source: "points",
+        filter: ["!", ["has", "point_count"]],
         paint: {
-          "circle-radius": [
-            "case",
-            ["boolean", ["feature-state", "hover"], false],
-            12,
-            6,
-          ],
-          "circle-color": [
-            "case",
-            ["boolean", ["feature-state", "hover"], false],
-            "#000",
-            "#2563eb", // 🔥 фіксований колір
-          ],
+          "circle-radius": 6,
+          "circle-color": "#2563eb",
           "circle-stroke-width": 2,
           "circle-stroke-color": "#fff",
         },
@@ -148,6 +202,7 @@ export default function Map() {
         id: "price-layer",
         type: "symbol",
         source: "points",
+        filter: ["!", ["has", "point_count"]],
         layout: {
           "text-field": ["get", "label"],
           "text-size": 12,
@@ -161,40 +216,55 @@ export default function Map() {
       });
 
       // 🔥 hover
-      let hoveredId: string | number | null = null;
-
-      map.on("mousemove", "points-layer", (e) => {
+      map.on("mousemove", "badge-bg", (e) => {
         if (!e.features?.length) return;
 
         const feature = e.features[0];
+        const id = feature.properties?.id;
+        map.getCanvas().style.cursor = "pointer";
+        // if (!id) return;
+        if (id === undefined || id === null) return;
 
-        if (!feature.id) return; // 🔥 FIX
-
-        if (hoveredId !== null) {
-          map.setFeatureState(
-            { source: "points", id: hoveredId },
-            { hover: false },
-          );
-        }
-
-        hoveredId = feature.id;
-
-        map.setFeatureState(
-          { source: "points", id: hoveredId },
-          { hover: true },
-        );
+        // 🔥 показуємо hover тільки для цієї точки
+        map.setFilter("hover-layer", ["==", ["get", "id"], id]);
       });
 
-      map.on("mouseleave", "points-layer", () => {
-        if (hoveredId !== null) {
-          map.setFeatureState(
-            { source: "points", id: hoveredId },
-            { hover: false },
-          );
-        }
-        hoveredId = null;
+      map.on("mouseleave", "badge-bg", () => {
+        // 🔥 ховаємо hover
+        map.getCanvas().style.cursor = "";
+        map.setFilter("hover-layer", ["==", ["get", "id"], -1]);
       });
-      setTimeout(() => map.resize(), 100); // 🔥 FIX
+
+      map.on("click", "clusters", (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["clusters"],
+        });
+
+        if (!features.length) return;
+
+        const clusterId = features[0].properties?.cluster_id;
+
+        const source = map.getSource("points") as mapboxgl.GeoJSONSource;
+
+        source.getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+
+          if (zoom === undefined || zoom === null) return;
+
+          map.easeTo({
+            center: (features[0].geometry as any).coordinates,
+            zoom: zoom,
+          });
+        });
+      });
+
+      map.on("mouseenter", "clusters", () => {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      map.on("mouseleave", "clusters", () => {
+        map.getCanvas().style.cursor = "";
+      });
     });
 
     return () => {
@@ -203,22 +273,17 @@ export default function Map() {
     };
   }, []);
 
-useEffect(() => {
-  if (!mapRef.current) return;
+  useEffect(() => {
+    if (!mapRef.current) return;
 
-  const map = mapRef.current;
+    const map = mapRef.current;
 
-  if (!map.isStyleLoaded()) return;
+    const source = map.getSource("points") as mapboxgl.GeoJSONSource;
 
-  const source = map.getSource("points") as mapboxgl.GeoJSONSource;
+    if (!source) return;
 
-  if (!source) return;
-
-  const geojson = buildGeoJSON();
-
-  source.setData(geojson);
-}, [dealType, propertyType]);
-
+    source.setData(buildGeoJSON());
+  }, [dealType, propertyType]);
 
   const filteredProperties = properties.filter(
     (p) => p.dealType === dealType && p.propertyType === propertyType,
